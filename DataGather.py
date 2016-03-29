1d8ec16fa6e45269aa86a947b3d21c9534ef2bc3
mankusn <mankusnick@yahoo.com>
2016-03-29 01:19:12 -0500

    Added ordered statistic function

--- DataGather.py
+++ DataGather.py
@@ -1,5 +1,17 @@
 import csv
 import os
+import operator
+
+#returns an ordered list of specific team-stat tuples
+def ordered_statistics(collection,stat,desc):
+    # if stat not in collection:
+    #     print "Stat does not exist"
+    #     return []
+    results = {}
+    for team in collection.keys():
+        entry = collection[team][stat]
+        results[team] = float(entry)
+    return sorted(results.items(), key=operator.itemgetter(1),reverse=desc)
 
 #gets a dictionary of statistics, team name is the key
 def import_csv_data(filename):
@@ -8,25 +20,23 @@ def import_csv_data(filename):
     for row in reader:
         key = row.pop('Tm')
         if key in dictionary:
-            # implement your duplicate row handling here
-            pass
+            continue
         dictionary[key] = row
     return dictionary
 def main():
 
 
     statistics={}
-    nfl = import_csv_data("NFL_Pass_Offense.csv")
-    print nfl
-    print nfl["New Orleans Saints"]["Att"]
     for file in os.listdir('.'):
         if file.endswith(".csv"):
             name =str(file)[:-4].replace("_"," ")
             statistics[name] = import_csv_data(file)
 
-    #Sample data access: statistics[#dataset][#teamname][#statistic]
-    print statistics["NFL Pass Defense"]["Denver Broncos"]['YdsL']
-
+    #Sample data access: statistics[#statistical set][#teamname][#statistic]
+    print type(statistics["NFL Pass Defense"]["Denver Broncos"]['YdsL'])
+    
+    a = ordered_statistics(statistics["NFL Pass Defense"], "TD",False)
+    print a[1][0]
 
 if __name__ == '__main__':
     main()
