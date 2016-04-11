a8f97e00e2ef9c0ab2bd7dc3fa1e348ac2d7b7e6
mankusn <mankusnick@yahoo.com>
2016-04-11 00:06:00 -0500

    Demo for Cav

--- Algorithm.py
+++ Algorithm.py
@@ -9,13 +9,16 @@ def main():
 
     nfl = fl.FootballLeague('.','NFL')
     ncaa = fl.FootballLeague('.', 'NCAA')
+    list1 = nfl.get_team("New England Patriots").find_match(ncaa)
+    list2 = ncaa.get_team("Texas A&M").find_match(nfl)
 
-    list = {}
-    list = nfl.get_team("New England Patriots").find_match(ncaa)
     print "Top 3 Recommendations for New England Patriots: "
     for num in range(0,3):
-        print sorted(list.items(), key=operator.itemgetter(1),reverse=True)[num]
+        print sorted(list1.items(), key=operator.itemgetter(1),reverse=True)[num]
 
+    print "\nTop 3 Recommendations for Texas A&M: "
+    for num in range(0,3):
+        print sorted(list2.items(), key=operator.itemgetter(1),reverse=True)[num]
 
 
 
